aadc1c247e419277a94b52ee8c553cd5378b1f5c
Yashshree bhavsar <yashashribh19@gmail.com>
2020-02-09 20:11:11 +0530

    CRUD operations module done with CSS,HTML,business logic

--- EHI-Project/src/app/contact-details/contact-details.component.ts
+++ EHI-Project/src/app/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactModel } from './contact-model'
 
 @Component({
   selector: 'app-contact-details',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailsComponent implements OnInit {
 
-  constructor() { }
+  contactDetailsList:any=[];
+  selectedIndex:any;
+  statusArr:any=['Active','Inactive'];
+  contactObj:any = new ContactModel();
+  prevData:any;
+  bAdd:boolean;
+  bUpdate:boolean;
 
-  ngOnInit() {
+  constructor(){  }
+
+  ngOnInit(){  }
+
+  addContact(){
+    let newData = JSON.parse(JSON.stringify(this.contactObj));
+    this.contactDetailsList.splice(0,0,newData);
+    this.prevData = newData;
+    this.selectedIndex = 0;
+    this.bAdd = true;
+  }
+
+  editDetails(ind){
+    this.prevData = JSON.parse(JSON.stringify(this.contactDetailsList[ind]));
+    this.selectedIndex = ind;
+    this.bUpdate = false;
+    this.bAdd = false;
+  }
+
+  deleteDetails(ind){
+    this.contactDetailsList.splice(ind,1);
+    this.selectedIndex = undefined;
+    this.bUpdate = false;
+    this.bAdd = false;
+  }
+
+  updateDetails(ind,formValid){    
+    formValid ? (this.selectedIndex = undefined,this.bAdd = false,this.bUpdate = false) : this.bUpdate = true;
+  }
+  
+  cancelDetails(ind){
+    this.bAdd ? this.contactDetailsList.shift() : this.contactDetailsList[ind] = this.prevData;
+    this.selectedIndex = undefined;
+    this.bAdd = false;
+    this.bUpdate = false;
   }
 
 }
