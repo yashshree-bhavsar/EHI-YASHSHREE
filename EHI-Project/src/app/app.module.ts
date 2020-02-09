a3beb44e5f5ec25fcbd8e4b748d4aa4a5843b054
Yashshree bhavsar <yashashribh19@gmail.com>
2020-02-09 20:07:58 +0530

    directive added

--- EHI-Project/src/app/app.module.ts
+++ EHI-Project/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { RegexValidatorDirective } from './directives/regex-validator.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactDetailsComponent
+    ContactDetailsComponent,
+    RegexValidatorDirective
   ],
   imports: [
     BrowserModule
